84e87f7e8900bb6a803a7c46409af0ef0876a9d5
Marcio122 <marcio122@outlook.com.br>
2024-01-18 12:47:14 +0100

    User Input Functions

---
+++ user_input_func.py
@@ -0,0 +1,9 @@
+my_name = input("Please enter your name: ")
+
+print(my_name)
+
+my_favorite_num = input("Please enter your favorite number: ")
+
+print(type(int(my_favorite_num)))
+
+print(my_name.title())

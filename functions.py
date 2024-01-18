633478740f281324c566597f786de1f190afa51c
Marcio122 <marcio122@outlook.com.br>
2024-01-18 02:53:20 +0100

    defining function and using result

--- functions.py
+++ functions.py
@@ -2,4 +2,13 @@ def add_numbers(a,b):
     sum = a + b
     print(sum)
 
-add_numbers(2, 2)
+
+def multiply_numbers(a, b):
+    result = a * b
+    return result
+
+result_of_multiplication = multiply_numbers(20, 5)
+print(result_of_multiplication)
+
+
+print(multiply_numbers(15,3))

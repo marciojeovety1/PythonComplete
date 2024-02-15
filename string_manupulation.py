ba8fc9b69e2aa7fb180663e36174ba3df3d9cdaa
Marcio122 <marcio122@outlook.com.br>
2024-02-16 00:01:52 +0100

    string manipulation

---
+++ string_manupulation.py
@@ -0,0 +1,13 @@
+long_str = "This is a very long string"
+
+single_quote_str = 'It\'s beautiful outside!'
+
+split_str = "This\n is\n a\n very\n long\n string\n"
+
+print(long_str)
+print(type(long_str) == str)
+print(id(long_str))
+
+print(single_quote_str)
+
+print(split_str)

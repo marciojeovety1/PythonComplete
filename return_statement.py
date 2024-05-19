4ce33e79f4bf285c1a1e802dfc6b0ccb91981b0b
Marcio122 <marcio122@outlook.com.br>
2024-05-19 20:40:11 +0100

    Returning specific functions

--- return_statement.py
+++ return_statement.py
@@ -1,10 +1,9 @@
-def multiply_numbers(a, b):
+def multiply_numbers(a,b):
     result = a * b
     return result
-    #print('This line is not executable')
+    #print("This line is not executed!")
 
-result_of_multiplication = multiply_numbers(20, 5)
+result_of_multiplication =  multiply_numbers(20, 5)
 print(result_of_multiplication)
 
-
-print(multiply_numbers(15,2))
+print(multiply_numbers(15, 2))

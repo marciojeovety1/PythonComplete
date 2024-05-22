721f3d71d6fc814fd90929bf12306f8ab6a7b1fe
Marcio122 <marcio122@outlook.com.br>
2024-05-22 11:59:21 +0100

    Statements

--- expressions.py
+++ expressions.py
@@ -10,3 +10,15 @@ num = int(input("Enter any number: "))
 print(num)
 print(type(num))
 
+# Statements
+
+a = 10
+
+if 10 > 2:
+    print(True)
+    
+def print_name(name):
+    print(name)
+    
+#import datetime
+#print(datetime.MAXYEAR)

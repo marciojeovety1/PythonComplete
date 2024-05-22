3e2f917b5e8421dccb3615da19f2ab8c75bb7873
Marcio122 <marcio122@outlook.com.br>
2024-05-22 12:12:44 +0100

    Finished task

--- expressions.py
+++ expressions.py
@@ -1,24 +1,32 @@
 # expressions
-a = 10 / 2
+#a = 10 / 2
 
-print(a)
+#print(a)
 
-res = print()
-print(res)
+#res = print()
+#print(res)
 
-num = int(input("Enter any number: "))
-print(num)
-print(type(num))
+#num = int(input("Enter any number: "))
+#print(num)
+#print(type(num))
 
 # Statements
 
-a = 10
+#a = 10
 
-if 10 > 2:
-    print(True)
+#if 10 > 2:
+#    print(True)
     
-def print_name(name):
-    print(name)
+#def print_name(name):
+#    print(name)
     
 #import datetime
 #print(datetime.MAXYEAR)
+
+# Task
+def my_name(name):
+    response = f'You are welcome {name}'
+    return response
+
+print(my_name('Marcio'))
+

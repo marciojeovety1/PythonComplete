361a811a9a4dfa481c6dcce258c1468b117c536d
Marcio122 <marcio122@outlook.com.br>
2024-05-25 23:46:44 +0100

    string manipulation

--- string_manupulation.py
+++ string_manupulation.py
@@ -1,18 +1,6 @@
-long_str = "This is a very long string"
-
-single_quote_str = 'It\'s beautiful outside!'
-
-split_str = "This\n is\n a\n very\n long\n string\n"
+long_str = 'This is a \n very long string'
 
 print(long_str)
+print(type(long_str))
 print(type(long_str) == str)
 print(id(long_str))
-
-print(single_quote_str)
-
-print(split_str)
-
-my_name = 'Marcio'
-print(id(my_name))
-
-print(id(my_name.upper()))

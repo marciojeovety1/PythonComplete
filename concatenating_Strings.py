f9ec7f8253ea0f61f638835a924874d72049e138
Marcio122 <marcio122@outlook.com.br>
2024-05-27 14:19:18 +0100

    Concatenating strings with + operator

--- concatenating_Strings.py
+++ concatenating_Strings.py
@@ -2,5 +2,5 @@ my_name = 'Marcio'
 my_hobby = 'running'
 time = 8
 
-#info = my_name + ' likes ' + my_hobby + ' at ' + str(time) + ' o\'clock'
-#print(info)
+info = my_name + ' likes ' + my_hobby + ' at ' + str(time) + ' o\'clock'
+print(info)

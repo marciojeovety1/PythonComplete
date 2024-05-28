a0ebab3c52cc5dccf01c83e82830ea0bc2698eb8
Marcio122 <marcio122@outlook.com.br>
2024-05-28 13:06:34 +0100

    integer manipulation

---
+++ integer_manipulation.py
@@ -0,0 +1,10 @@
+my_num = 1_000_000_000_000
+other_num = 100
+
+res = my_num - other_num
+print(res)
+
+input_str =  int(input("Enter any number: "))
+input_num = int(input_str)
+print(input_num)
+print(type(input_str))

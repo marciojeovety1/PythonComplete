e8b9890469a65f79dfc9192125c1cff59d3ccf0c
Marcio122 <marcio122@outlook.com.br>
2024-05-29 09:38:07 +0100

    Task for dictionary

---
+++ task_for_dict.py
@@ -0,0 +1,16 @@
+my_list = {}
+
+
+ask_user = input('Enter the first key: ')
+ask_user2 = input('Enter the second key: ')
+ask_user_3 = input('Enter the third key: ')
+
+user_value = input('Enter the first value: ')
+user_value2 = input('Enter the second value: ')
+user_value_3 = input('Enter the third value: ')
+
+my_list[ask_user] = user_value
+my_list[ask_user2] = user_value2
+my_list[ask_user_3] = user_value_3
+
+print(my_list)

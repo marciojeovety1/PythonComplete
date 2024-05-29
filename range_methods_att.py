9d6d7b61f93f9cd1253e0fcaf83fd7116b1d6473
Marcio122 <marcio122@outlook.com.br>
2024-05-29 12:53:34 +0100

    Range methods and attributes

---
+++ range_methods_att.py
@@ -0,0 +1,19 @@
+odd_nums = range(1, 100, 2)
+
+print(odd_nums.index(51))
+
+print(odd_nums.start)
+print(odd_nums.stop)
+print(odd_nums.step)
+
+
+my_range = range(5, 8)
+
+print(my_range.start)
+print(my_range.stop)
+print(my_range.step)
+
+print(list(odd_nums))
+print(tuple(odd_nums))
+print(set(odd_nums))
+#TypeError for dict

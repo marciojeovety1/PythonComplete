cf7295295a64fec8eb911d1c89973b8c1a45dada
Marcio122 <marcio122@outlook.com.br>
2024-05-29 10:03:08 +0100

    tuple manipulation

---
+++ tuple_manipulation.py
@@ -0,0 +1,20 @@
+screen_res = (1920, 1080)
+screen_info = (True, 'Samsung', 150)
+print(screen_res)
+print(type(screen_res))
+print(isinstance(screen_res, tuple))
+
+print(screen_res.index(1080))
+
+print(screen_res.count(1080))
+
+screen_res_list = list(screen_res)
+
+screen_res_list[0] = 1444
+print(screen_res_list)
+
+screen_res = tuple(screen_res_list)
+print(screen_res)
+
+screen_data = screen_res + screen_info
+print(screen_data)
